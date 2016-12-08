d7aaef5328a9ee6e5f83c569b2e85cb7d5169d24
James Matsumura <james.matsumura@gmail.com>
2016-12-08 17:02:06 -0500

    BAM analysis step done. Need to filter reads for SPAdes now.

--- analyze_bam.py
+++ analyze_bam.py
@@ -2,28 +2,49 @@
 
 # This script will parse through a BAM file resulting from a paired-end 
 # alignment and extract all query sequence IDs that meet a certain percent
-# identity threshold (which the user can set). The output will be a tab-delimited 
-# file with the first column being the read ID, the second column being the
-# %ID found (calculated by CIGAR string in SAM/BAM file), and all subsequent
-# columns being the reference that the read aligned to.  
+# identity threshold (which the user can set). 
+#
+# The output will be two tab-delimited files:
+# 1) prefix_read_map.tsv
+# A map file where a given read is a key and it maps to all the references
+# that it hit. The first column is the read ID, every subsequent column is a
+# combination of %ID|length|referenceID for all the reference alignments 
+# that passed the threshold. Note that %ID and length are included so that
+# these values can help determine which read may have a better alignment
+# given a similar %ID. 
+#
+# 2) prefix_ref_map.tsv
+# Similar to the first, except this one uses the reference ID as the main
+# map point. The subsequent columns will consist of %ID|readID. 
+# 
+# While these two files are similar, they're more useful in separate cases. The
+# first is what will be used downstream here while the second can be used to 
+# check for which reference contigs were able to recruit the most reads. 
 #
 # Run the script using a command like this:
-# python3 analyze_bam.py -bam /path/to/bowtie_out.bam (-threshold 90) -o /path/to/output.tsv
+# python3 analyze_bam.py -bam /path/to/bowtie_out.bam (-threshold 90) -o /path/to/out_prefix
 #
 # Author: James Matsumura
 
 import sys,re,argparse,pysam
+from collections import defaultdict
 
 def main():
 
     parser = argparse.ArgumentParser(description='Script to isolate all reads and where they aligned to given a BAM file.')
     parser.add_argument('-bam', type=str, required=True, help='Path to a BAM file derived from Bowtie2.')
     parser.add_argument('-threshold', type=str, required=False, help='Minimum %ID threshold to retain (entering 95 means %95 minimum %ID). Defaults to %80.')
-    parser.add_argument('-o', type=str, required=True, help='Path to where the output TSV should go.')
+    parser.add_argument('-o', type=str, required=True, help='Path to prefix of where the two output TSV files should go.')
     args = parser.parse_args()
 
     i = pysam.AlignmentFile(args.bam,'r')
-    o = open(args.o,'w')
+
+    read_map = args.o + "_read_map.tsv"
+    ref_map = args.o + "_ref_map.tsv"
+    o1 = open(read_map,'w')
+    o2 = open(ref_map,'w')
+
+    rd_map,rf_map = (defaultdict(list) for i in range(2)) # establish these dicts as holding lists
 
     threshold = 80  # minimum cutoff to include a read
     if args.threshold: # user specificying threshold
@@ -39,9 +60,13 @@ def main():
             percent_id = calculate_percent_id(cigar,length)
             
             if percent_id >= threshold: # good enough %ID? go on...
-                que = read.query_name # grab read ID
+                rea = read.query_name # grab read ID
                 ref = read.reference_name # grab ID of what the ID aligned to
+                rd_map[rea].append("{0}|{1}|{2}".format(percent_id,length,ref))
+                rf_map[ref].append("{0}|{1}|{2}".format(percent_id,length,rea))
 
+    write_tsv(rd_map,o1)
+    write_tsv(rf_map,o2)
 
 # Calculates % identity given info from a CIGAR string
 # Expects a tuple based on the pysam documentation (http://pysam.readthedocs.io/en/latest/api.html) where...
@@ -55,6 +80,9 @@ def main():
 # 7 is =
 # 8 is X
 # Thus, need to aggregate all 0/M and then divide by total alignment length.
+# Arguments:
+# cigar = CIGAR tuple
+# length = length of the alignment (needed to calculate %ID)
 def calculate_percent_id(cigar,length):
     
     matches,percent_id = (0 for i in range(2))
@@ -66,6 +94,17 @@ def calculate_percent_id(cigar,length):
     percent_id = (100 * (matches/length))
 
     return float("{0:.2f}".format(percent_id))
+
+# Function which takes the built dictionaries and writes the TSVs
+# Arguments:
+# map = dictionary of lists where the key is either the read/ref and the list
+# contains all the ref/reads that have alignment to the key
+# outfile = path to the TSV outfile
+def write_tsv(map,outfile):
+    for entry in map:
+        mapped_vals = "\t".join(map[entry])
+        tsv_line = "{0}\t{1}\n".format(entry,mapped_vals)
+        outfile.write(tsv_line)
 
 if __name__ == '__main__':
     main()
