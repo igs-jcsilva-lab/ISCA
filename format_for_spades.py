3df002d13df441888da8e4d5b59d4fa5ebec5b9c
James Matsumura <james.matsumura@gmail.com>
2016-12-16 11:10:51 -0500

    Syntax issues.

--- format_for_spades.py
+++ format_for_spades.py
@@ -7,7 +7,7 @@
 # be arbitrary and start at 1 climbing until it hits the number of directories made.
 #
 # Run the script using a command like this:
-# python3 analyze_bam.py -i /path/to/ref_map.tsv -path /path/to/ref_dirs -out /path/to/out_map.tsv
+# python3 analyze_bam.py -i /path/to/ref_map.tsv -path /path/to/ref_dirs -spades_path /path/to/spades/dirs -out /path/to/out_map.tsv
 #
 # Author: James Matsumura
 
@@ -16,7 +16,7 @@ from shared_fxns import make_directory
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Script to generate stats given output from analyze_bam.py and filter a set of paired-end FASTQ reads.')
+    parser = argparse.ArgumentParser(description='Script to set up for SPAdes alignment on a grid.')
     parser.add_argument('-refs', type=str, required=True, help='Path to *_ref_map.tsv output from analyze_bam.py.')
     parser.add_argument('-path', type=str, required=True, help='Path to the the directory preceding all the ref directories (e.g. for "/path/to/ref123" put "/path/to" as the input).')
     parser.add_argument('-spades_path', type=str, required=True, help='Path to the the directory to initialize directories for all the SPAdes output.')
@@ -33,10 +33,6 @@ def main():
             line = line.rstrip()
             ref = line.split('\t') # really just want the first column which is the ref ID
 
-            # First make a new output directory for all the SPAdes assembly files
-            spades_out_dir = "{0}/{1}".format(args.spades_path,ref[0])
-            make_directory(spades_out_dir)
-
             # Now rename the original directory so that it can be iterated over in a grid
             # job. 
             old_dir = "{0}/{1}".format(args.path,ref[0])
@@ -49,10 +45,15 @@ def main():
                     raise
             else:
                 ref_map[ref[0]] = id
+
+                # Make a new output directory for all the SPAdes assembly files
+                spades_out_dir = "{0}/{1}".format(args.spades_path,id)
+                make_directory(spades_out_dir)
+
                 id += 1 # if no exception, it was renamed and need a new ID
 
     # Now generate a map to know which directories correlate to what IDs
-    with open(args.out,'w') as outfile:
+    with open(args.outfile,'w') as outfile:
         for k,v in ref_map.items():
             outfile.write("{0}\t{1}\n".format(k,v))
 
