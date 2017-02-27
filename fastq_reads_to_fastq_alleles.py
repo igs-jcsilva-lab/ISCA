ec94fa3734b112ec40f366145de2003b388c0dd7
James Matsumura <james.matsumura@gmail.com>
2017-02-27 15:47:38 -0500

    Allow GSNAP to be used in place of Bowtie2 at the alignment phase.

--- fastq_reads_to_fastq_alleles.py
+++ fastq_reads_to_fastq_alleles.py
@@ -35,6 +35,7 @@ def main():
     parser.add_argument('-ab_read_map', type=str, required=True, help='Path to *_read_map.tsv output from analyze_bam.py.')
     parser.add_argument('-fastq', type=str, required=True, help='Path to the original FASTQ file prefix of paired-end reads (e.g., enter ABC.123 for pairs ABC.123.1+ABC.123.2). MUST be gunzipped.')
     parser.add_argument('-filter', type=str, required=True, help='Either "yes" or "no" for removing discrepancies + multi-locus mapping reads.')
+    parser.add_argument('-aligner', type=str, required=True, help='Whether Bowtie2 or GSNAP was used to align. Enter either "bowtie2" or "gsnap".')
     parser.add_argument('-out', type=str, required=True, help='Path to where the output directory for the FASTQs to go.')
     args = parser.parse_args()
 
@@ -51,96 +52,113 @@ def main():
     
     unique_refs = set() # make directories now for where all the reads will go
 
-    # This first iteration only cares about grabbing all mates and their reference alignment info
-    with open(args.ab_read_map,'r') as reads:
-        for line in reads: 
-            
-            line = line.rstrip()
-            ele = line.split('\t')
-            
-            if ele[0][-1] == "1": # read mate 1
+    if args.aligner == 'bowtie2':
+        # This first iteration only cares about grabbing all mates and their reference alignment info
+        with open(args.ab_read_map,'r') as reads:
+            for line in reads: 
+                
+                line = line.rstrip()
+                ele = line.split('\t')
+                
+                if ele[0][-1] == "1": # read mate 1
+                    for j in range(1,len(ele)):
+                        alignment = ele[j].split('|') # split the alignment data
+                        ref = alignment[2].split('.') # split the reference name
+                        ref_loc = ref[1] # grab just the base reference locus
+                        # don't double up on references (possible if mapping to same locus from different samples)
+                        if ref_loc not in r1[ele[0]]: 
+                            r1[ele[0]].append(ref_loc)
+                else: # read mate 2
+                    for j in range(1,len(ele)):
+                        alignment = ele[j].split('|')
+                        ref = alignment[2].split('.')
+                        ref_loc = ref[1]
+                        if ref_loc not in r2[ele[0]]:
+                            r2[ele[0]].append(ref_loc)
+
+        shared_id = "" # id in the format of ABC.123 for pairs ABC.123.1 + ABC.123.2
+        checked_ids,ref_dirs = (set() for j in range(2)) # set to speed up processing of R2 if already covered by R1
+
+        # Now, iterate over each dict of mates and filter if required
+        for read in r1: # mate 1
+            shared_id = read[:-2]
+            mate_id = shared_id + ".2"
+
+            # Generate stats regardless of filtering or not, can help the user decide if they should
+            count_val = verify_alignment(r1[read],r2[mate_id])
+            counts[count_val] += 1
+
+            if filter == "yes" and count_val == "single_map": # need to isolate reads that only map once
+                
+                # If a single map value, know that both reads share the same locus
+                if not r1[read]: # if R1 didn't map, means R2 did
+                    ids_to_keep[shared_id].append(r2[mate_id][0])
+                elif not r2[mate_id]: # same as above, if R2 didn't map, means R1 did
+                    ids_to_keep[shared_id].append(r1[read][0])
+                else: # else, they both mapped to the same locus and can use either value
+                    ids_to_keep[shared_id].append(r1[read][0])
+
+                unique_refs.add(ids_to_keep[shared_id][0]) # add the single locus
+
+            else: # no filter needed, add all distinct loci found per read
+
+                for ref in r1[read]:
+                    ids_to_keep[shared_id].append(ref)
+                    unique_refs.add(ref) # add all loci
+
+                for ref in r2[mate_id]:
+                    if ref not in ids_to_keep[shared_id]: # make sure not to double up on loci across mates
+                        ids_to_keep[shared_id].append(ref)
+                        unique_refs.add(ref) # add all loci
+
+            checked_ids.add(shared_id) # identify these as looked at before going into r2 dict
+
+        for read in r2: # mate 2, only check if the mate wasn't caught by the r1 dict
+            shared_id = read[:-2]
+
+            if shared_id not in checked_ids: # if not checked using mate 1, verify now.
+
+                mate_id = shared_id + ".1"   
+                count_val = verify_alignment(r1[mate_id],r2[read])
+                counts[count_val] += 1
+
+                # If we are here, the read was not found in R1. Thus, get loci strictly from R2.
+                if filter == "yes" and count_val == "single_map": 
+                    ids_to_keep[shared_id].append(r2[read][0])
+                    unique_refs.add(ids_to_keep[shared_id][0]) # add the single locus
+
+                else: # Again, was not found in R1 so we know all loci are from R2. 
+                    for ref in r2[read]:
+                        ids_to_keep[shared_id].append(ref)
+                        unique_refs.add(ref) # add the single locus
+
+        # At this point, ids_to_keep now has a dictionary mapping all read IDs to loci that
+        # they aligned to. This is all that's needed to build a set of directories that house
+        # reads just mapping to those loci for use in assembly.
+
+        r1,r2,checked_ids = (None for j in range(3)) # done with these, free up some memory
+
+        # give the user some idea of how much they are potentially filtering out
+        out_stats = output + ".stats"
+        with open(out_stats,'w') as stats_file:
+            for k,v in counts.items():
+                stats_file.write("{0} read-pairs have a {1}.\n".format(v,k))
+
+    elif args.aligner == 'gsnap':
+        with open(args.ab_read_map,'r') as reads:
+            for line in reads: 
+                
+                line = line.rstrip()
+                ele = line.split('\t')
+                
                 for j in range(1,len(ele)):
                     alignment = ele[j].split('|') # split the alignment data
                     ref = alignment[2].split('.') # split the reference name
                     ref_loc = ref[1] # grab just the base reference locus
                     # don't double up on references (possible if mapping to same locus from different samples)
-                    if ref_loc not in r1[ele[0]]: 
-                        r1[ele[0]].append(ref_loc)
-            else: # read mate 2
-                for j in range(1,len(ele)):
-                    alignment = ele[j].split('|')
-                    ref = alignment[2].split('.')
-                    ref_loc = ref[1]
-                    if ref_loc not in r2[ele[0]]:
-                        r2[ele[0]].append(ref_loc)
-
-    shared_id = "" # id in the format of ABC.123 for pairs ABC.123.1 + ABC.123.2
-    checked_ids,ref_dirs = (set() for j in range(2)) # set to speed up processing of R2 if already covered by R1
-
-    # Now, iterate over each dict of mates and filter if required
-    for read in r1: # mate 1
-        shared_id = read[:-2]
-        mate_id = shared_id + ".2"
-
-        # Generate stats regardless of filtering or not, can help the user decide if they should
-        count_val = verify_alignment(r1[read],r2[mate_id])
-        counts[count_val] += 1
-
-        if filter == "yes" and count_val == "single_map": # need to isolate reads that only map once
-            
-            # If a single map value, know that both reads share the same locus
-            if not r1[read]: # if R1 didn't map, means R2 did
-                ids_to_keep[shared_id].append(r2[mate_id][0])
-            elif not r2[mate_id]: # same as above, if R2 didn't map, means R1 did
-                ids_to_keep[shared_id].append(r1[read][0])
-            else: # else, they both mapped to the same locus and can use either value
-                ids_to_keep[shared_id].append(r1[read][0])
-
-            unique_refs.add(ids_to_keep[shared_id][0]) # add the single locus
-
-        else: # no filter needed, add all distinct loci found per read
-
-            for ref in r1[read]:
-                ids_to_keep[shared_id].append(ref)
-                unique_refs.add(ref) # add all loci
-
-            for ref in r2[mate_id]:
-                if ref not in ids_to_keep[shared_id]: # make sure not to double up on loci across mates
-                    ids_to_keep[shared_id].append(ref)
-                    unique_refs.add(ref) # add all loci
-
-        checked_ids.add(shared_id) # identify these as looked at before going into r2 dict
-
-    for read in r2: # mate 2, only check if the mate wasn't caught by the r1 dict
-        shared_id = read[:-2]
-
-        if shared_id not in checked_ids: # if not checked using mate 1, verify now.
-
-            mate_id = shared_id + ".1"   
-            count_val = verify_alignment(r1[mate_id],r2[read])
-            counts[count_val] += 1
-
-            # If we are here, the read was not found in R1. Thus, get loci strictly from R2.
-            if filter == "yes" and count_val == "single_map": 
-                ids_to_keep[shared_id].append(r2[read][0])
-                unique_refs.add(ids_to_keep[shared_id][0]) # add the single locus
-
-            else: # Again, was not found in R1 so we know all loci are from R2. 
-                for ref in r2[read]:
-                    ids_to_keep[shared_id].append(ref)
-                    unique_refs.add(ref) # add the single locus
-
-    # At this point, ids_to_keep now has a dictionary mapping all read IDs to loci that
-    # they aligned to. This is all that's needed to build a set of directories that house
-    # reads just mapping to those loci for use in assembly.
-
-    r1,r2,checked_ids = (None for j in range(3)) # done with these, free up some memory
-
-    # give the user some idea of how much they are potentially filtering out
-    out_stats = output + ".stats"
-    with open(out_stats,'w') as stats_file:
-        for k,v in counts.items():
-            stats_file.write("{0} read-pairs have a {1}.\n".format(v,k))
+                    if ref_loc not in ids_to_keep[ele[0]]: # just one dict here since gsnap doesn't capture read suffix
+                        ids_to_keep[ele[0]].append(ref_loc)
+                        unique_refs.add(ref_loc)
 
     # Write out all the directories
     for ref in unique_refs:
