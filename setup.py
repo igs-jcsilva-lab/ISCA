f786410da6288cd1d7d7bfcf86b615d76ee35447
James Matsumura <james.matsumura@gmail.com>
2017-06-02 18:48:33 -0500

    Revert "Fixing some dependencies."
    This reverts commit 5c87eba3cd3a4ad81fa4c24cf3a4d95db61dab8c.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@
 #
 # Author: James Matsumura
 
-import argparse
 from shared_fxns import make_directory
 
 def main():
@@ -20,16 +19,16 @@ def main():
     args = parser.parse_args()
 
     # directories for the grid to output logs and errors
-    make_directory("{0}/grid_out".format(args.output_location))
-    make_directory("{0}/grid_err".format(args.output_location))
+    make_directory("{0}/grid_out".formatargs.output_location)
+    make_directory("{0}/grid_err".formatargs.output_location)
 
-    make_directory("{0}/gsnap_idx".format(args.output_location)) # stores GSNAP index
-    make_directory("{0}/smalt_idx".format(args.output_location)) # stores SMALT index
-    make_directory("{0}/sam".format(args.output_location)) # alignment output
-    make_directory("{0}/reads".format(args.output_location)) # individual read sets per locus
-    make_directory("{0}/spades_assemblies".format(args.output_location)) # assembly method 1 results
-    make_directory("{0}/hga_assemblies".format(args.output_location)) # assembly method 2 results
-    make_directory("{0}/alignments".format(args.output_location)) # alignment results
+    make_directory("{0}/gsnap_idx".formatargs.output_location) # stores GSNAP index
+    make_directory("{0}/smalt_idx".formatargs.output_location) # stores SMALT index
+    make_directory("{0}/sam".formatargs.output_location) # alignment output
+    make_directory("{0}/reads".formatargs.output_location) # individual read sets per locus
+    make_directory("{0}/spades_assemblies".formatargs.output_location) # assembly method 1 results
+    make_directory("{0}/hga_assemblies".formatargs.output_location) # assembly method 2 results
+    make_directory("{0}/alignments".formatargs.output_location) # alignment results
 
 if __name__ == '__main__':
     main()
